0497cdfaa396065ff4a07c7d3b027d0c4b4284b3
TallCricket <shivankp900@gmail.com>
2026-03-27 13:27:23 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -10,21 +10,25 @@ import Privacy from './pages/Privacy'
 import Terms from './pages/Terms'
 import Contact from './pages/Contact'
 import NotFound from './pages/NotFound'
+import HashtagPage from './pages/HashtagPage'
+import SearchPage from './pages/SearchPage'
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/"          element={<Socialgati />} />
-      <Route path="/news"      element={<NewsTally />} />
-      <Route path="/shorts"    element={<Shorts />} />
-      <Route path="/news/:id"  element={<NewsOpen />} />
-      <Route path="/alerts"    element={<Alerts />} />
-      <Route path="/profile"   element={<Profile />} />
-      <Route path="/about"     element={<About />} />
-      <Route path="/privacy"   element={<Privacy />} />
-      <Route path="/terms"     element={<Terms />} />
-      <Route path="/contact"   element={<Contact />} />
-      <Route path="*"          element={<NotFound />} />
+      <Route path="/"            element={<Socialgati />} />
+      <Route path="/news"        element={<NewsTally />} />
+      <Route path="/shorts"      element={<Shorts />} />
+      <Route path="/news/:id"    element={<NewsOpen />} />
+      <Route path="/alerts"      element={<Alerts />} />
+      <Route path="/search"      element={<SearchPage />} />
+      <Route path="/profile"     element={<Profile />} />
+      <Route path="/hashtag/:tag" element={<HashtagPage />} />
+      <Route path="/about"       element={<About />} />
+      <Route path="/privacy"     element={<Privacy />} />
+      <Route path="/terms"       element={<Terms />} />
+      <Route path="/contact"     element={<Contact />} />
+      <Route path="*"            element={<NotFound />} />
     </Routes>
   )
 }
